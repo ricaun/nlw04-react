b72aee27c48edc6c678a7909633fec6d451594f8
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-02-24 11:24:55 -0300

    First Deploy

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # nlw04-react
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+## Create Project
+
+```bash
+yarn create next-app projectName
+```
+
+## Update to Typescript
+
+```bash
+yarn add typescript @types/react @types/react-dom @types/node -D
+```
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
---
+++ src/components/Countdown.tsx
@@ -0,0 +1,92 @@
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Countdown.module.css'
+
+let countdownTimeout: NodeJS.Timeout;
+
+export function Countdown() {
+    const { startNewChallenge } = useContext(ChallengesContext);
+
+    const startTimer = 5;
+    const [time, setTime] = useState(startTimer);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinish, setHasFinish] = useState(false);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+    function startCountdown() {
+        setIsActive(true);
+    }
+
+    function resetCountdown() {
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setTime(startTimer);
+    }
+
+    useEffect(() => {
+        if (isActive && time > 0) {
+            countdownTimeout = setTimeout(() => {
+                setTime(time - 1);
+            }, 1000)
+        } else if (isActive && time == 0) {
+            setHasFinish(true);
+            setIsActive(false);
+            startNewChallenge();
+        }
+
+    }, [isActive, time])
+
+    return (
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
+
+            {hasFinish ? (
+                <button
+                    disabled
+                    className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                    onClick={resetCountdown}
+                >
+                    Ciclo encerrado
+                </button>
+            ) : (
+                    <>
+                        {isActive ? (
+                            <button
+                                type="button"
+                                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                                onClick={resetCountdown}
+                            >
+                                Abanonar Ciclo
+                            </button>
+                        ) : (
+                                <button
+                                    type="button"
+                                    className={styles.countdownButton}
+                                    onClick={startCountdown}
+                                >
+                                    Iniciar Ciclo
+                                </button>
+                            )}
+                    </>
+                )}
+
+
+
+        </div>
+    );
+}
